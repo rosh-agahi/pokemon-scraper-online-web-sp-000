ee1bda804d72030bb9c33dc7f374830b64d59809
Roshanak Agahi <rosh.agahi@gmail.com>
2019-10-09 19:05:23 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -3,7 +3,7 @@ class Pokemon
   attr_accessor :name, :type
   attr_reader :id, :db 
   
-  def initialize(name:, type:, db:, id: = nil)
+  def initialize(name:, type:, db:, id: => nil)
     @name = name
     @type = type
     @db = db
